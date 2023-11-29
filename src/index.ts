fc047e058b0e1abd0fed6402c7f572cf63482609
Ronald A. Richardson <me@ron.dev>
2023-11-29 13:59:57 +0800

    plugin build is working, added two endpoints for querying and listing published extensions

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,41 @@
 import { Logger, IPluginMiddleware, IBasicAuth, IStorageManager, PluginOptions } from '@verdaccio/types';
 import { Router, Request, Response, NextFunction, Application } from 'express';
-import { CustomConfig } from '../types/index';
+import { CustomConfig } from './types/index';
 
 export default class FleetbaseExtensionsMiddlewarePlugin implements IPluginMiddleware<CustomConfig> {
-    public logger: Logger;
-    public foo: string;
-    public constructor(config: CustomConfig, options: PluginOptions<CustomConfig>) {
-        this.foo = config.foo !== undefined ? config.strict_ssl : true;
-        this.logger = options.logger;
-    }
+    public constructor(config: CustomConfig, options: PluginOptions<CustomConfig>) {}
 
-    public register_middlewares(
-        app: Application,
-        auth: IBasicAuth<CustomConfig>,
-        /* eslint @typescript-eslint/no-unused-vars: off */
-        _storage: IStorageManager<CustomConfig>
-    ): void {
-        // eslint new-cap:off
+    public register_middlewares(app: Application, auth: IBasicAuth<CustomConfig>, storage: IStorageManager<CustomConfig>): void {
         const router = Router();
-        router.get('/api/extensions', (req: Request, res: Response & { report_error?: Function }, next: NextFunction): void => {
-            const query = req.query.q || '';
-
-            storage.get((err, packages) => {
+        router.get('/list', (req: Request, res: Response & { report_error?: Function }, next: NextFunction): void => {
+            storage.getLocalDatabase((err, packages) => {
                 if (err) {
-                    return res.status(500).send('Internal Server Error');
+                    return next(err);
                 }
 
-                let filteredExtensions = packages;
-
-                // Implement filtering logic based on the query
-                if (query) {
-                    filteredExtensions = packages.filter((pkg) => pkg.name.includes(query) || (pkg.description && pkg.description.includes(query)));
-                }
-
-                // Transform the filtered packages data
-                const extensions = filteredExtensions.map((pkg) => ({
-                    name: pkg.name,
-                    description: pkg.description,
-                    // Include other relevant details
-                }));
-
-                res.json(extensions);
+                res.json(packages);
             });
-            next();
+        });
+
+        router.get('/search', (req: Request, res: Response & { report_error?: Function }, next: NextFunction): void => {
+            const keyword = req.query.q as string;
+            const stream = storage.search(keyword, {});
+            const packages: any[] = [];
+
+            stream.on('data', (data) => {
+                // Process each chunk of data
+                packages.push(data);
+            });
+
+            stream.on('error', (err) => {
+                // Handle any errors during streaming
+                next(err);
+            });
+
+            stream.on('end', () => {
+                // All data has been received
+                res.json(packages);
+            });
         });
 
         app.use('/-/flb/extensions', router);
